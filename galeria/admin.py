b0b1cd3cd8adb4ae244477a1f5a8e5703ce55d80
EvErToN <evertonflorenciodasilva@outlook.com>
2024-05-24 00:25:21 -0300

    o erro das imagens ainda continua mas terminei de configurar o admin agora estou no buscar

--- galeria/admin.py
+++ galeria/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from galeria.models import Fotografia
 # Register your models here.
 class listandoFotografia(admin.ModelAdmin):
-    list_display = ('id','nome','legenda','descricao')
+    list_display = ('id','nome','legenda','descricao', 'legenda')
     list_display_links =('id','nome')
     search_fields=('nome',)
     list_filter = ('categoria',)
--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 class Fotografia (models.Model):
@@ -13,7 +15,9 @@ class Fotografia (models.Model):
     legenda = models.CharField(max_length=100, null=False,blank=False)
     categoria = models.CharField(max_length=100,choices=OPÇÕES_CATEGORIA,default='')
     descricao = models.TextField(null=False, blank= True)
-    foto = models.CharField(max_length=150, null=False,blank=False)
+    foto = models.ImageField(upload_to='fotos/%Y/%m/%d', blank=True)
+    publica = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now,blank=False)
 
     def __str__(self):
         return f"Fotografia [nome = {self.nome}]"
